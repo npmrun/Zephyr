e6bd237c8281f9fafe2a5f87a27a4274e73b2b9d
npmrun <1549469775@qq.com>
2024-11-16 02:23:55 +0800

    chore: 增加格式化工具

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,34 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution")
 
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:vue/vue3-recommended',
-    '@electron-toolkit',
-    '@electron-toolkit/eslint-config-ts/eslint-recommended',
-    '@vue/eslint-config-typescript/recommended',
-    '@vue/eslint-config-prettier'
-  ],
-  rules: {
-    'vue/require-default-prop': 'off',
-    'vue/multi-word-component-names': 'off'
-  }
+    extends: [
+        "eslint:recommended",
+        "plugin:vue/vue3-recommended",
+        "@electron-toolkit",
+        "@electron-toolkit/eslint-config-ts/eslint-recommended",
+        "@vue/eslint-config-typescript/recommended",
+        "@vue/eslint-config-prettier",
+    ],
+    rules: {
+        "vue/require-default-prop": "off",
+        "vue/multi-word-component-names": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "prettier/prettier": [
+            "error",
+            {
+                tabWidth: 4,
+                useTabs: false,
+                semi: false,
+                singleQuote: false,
+                trailingComma: "all",
+                bracketSpacing: true,
+                arrowParens: "avoid",
+                printWidth: 140,
+                htmlWhitespaceSensitivity: "ignore",
+                proseWrap: "preserve",
+                endOfLine: "auto",
+            },
+        ],
+    },
 }
